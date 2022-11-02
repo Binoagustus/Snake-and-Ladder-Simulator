adde57443ec15bd00f3e01d4dd22dd7e113ae701
Binoagustus <binoagustus@gmail.com>
2022-11-03 01:04:26 +0530

    Roll a dice

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,9 +1,12 @@
 package com.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeandLadder {
 	int[] board = new int[100];
 	final static int WINPOINT = 100;
 	int position = 1;
+	int num;
 	
 	public void simulator() {
 		for(int i = 0; i < 100; i++) {
@@ -11,10 +14,18 @@ public class SnakeandLadder {
 		}
 	}
 	
+	public int dieRoll() {
+		    Random dice = new Random();
+		    num=dice.nextInt(7);
+		    return (num==0?1:num);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Snake and Ladder Simulator");
 		SnakeandLadder sim = new SnakeandLadder();
 		sim.simulator();
+		int die = sim.dieRoll();
+		System.out.println(die);
 	}
 
 }

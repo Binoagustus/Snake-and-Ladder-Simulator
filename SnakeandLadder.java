e42a8fed2d4991d10eb98ad6d40075d76ab09c22
Binoagustus <binoagustus@gmail.com>
2022-11-03 16:44:03 +0530

    loop till player reach 100th position

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,6 +1,7 @@
 package com.snakeandladder;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class SnakeandLadder {
 	final static int WINPOINT = 100;
@@ -14,33 +15,34 @@ public class SnakeandLadder {
 		
 			if(playerPosition > WINPOINT) {
 				playerPosition = playerPosition - dieValue;
-				System.out.println("Player is at the position "+playerPosition);
+				System.out.println("\nPlayer is back at the position "+playerPosition+"\n");
 				return playerPosition;		
 			}
 			
 			//ladder
 			if(playerPosition == 8) {
 				playerPosition = playerPosition + 22;
-				System.out.println("Ladder upward to "+playerPosition+" from position 8");
+				System.out.println("\nLadder upward to "+playerPosition+" from position 8\n");
 				return playerPosition;
 			} else if(playerPosition == 87) {
 				playerPosition = playerPosition + 11;
-				System.out.println("Ladder upward to "+playerPosition+" from position 87");
+				System.out.println("\nLadder upward to "+playerPosition+" from position 87\n");
 				return playerPosition;
 			} 
 			
 			//snake
 			if(playerPosition == 97) {
 				playerPosition = playerPosition - 19;
-				System.out.println("Snake bite you");
-				System.out.println("Move backward to "+playerPosition+" from position 97");
+				System.out.println("\nSnake bite you");
+				System.out.println("Moved backward to "+playerPosition+" from position 97\n");
 				return playerPosition;	
 			} else if(playerPosition == 48) {
 				playerPosition = playerPosition - 22;
-				System.out.println("Snake bite you");
-				System.out.println("Move backward to "+playerPosition+" from position 48");
+				System.out.println("\nSnake bite you");
+				System.out.println("Moved backward to "+playerPosition+" from position 48\n");
 				return playerPosition;	
 			}
+			
 
 			return playerPosition;
 	}
@@ -53,10 +55,21 @@ public class SnakeandLadder {
 	
 	public static void main(String[] args) {
 		System.out.println("Snake and Ladder Simulator");
+		int count  = 0;
+		int position;
 		SnakeandLadder sim = new SnakeandLadder();
-		int dice = sim.dieRoll();
-	    sim.simulator(dice);
-	    
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter r to roll the dice ");
+		String roll = sc.next();
+		do {
+			int dice = sim.dieRoll();
+			count++;
+			position = sim.simulator(dice);
+			
+			if(position == WINPOINT) {
+				System.out.println("You won the match");
+				break;
+			}
+		} while("r".equals(roll)); 
 	}
-
 }
